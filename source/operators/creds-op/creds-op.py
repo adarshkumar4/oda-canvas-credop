11d7901729ebce1384cf2d983845fb0dfb8c1b26
adarshkumar4 <adarshkumar.mishra@vodafone.com>
2024-12-27 15:31:06 +0000

    logging added

--- source/operators/creds-op/creds-op.py
+++ source/operators/creds-op/creds-op.py
@@ -1,6 +1,7 @@
 import kopf
 import requests
 import base64
+import logging
 import kubernetes.client
 from kubernetes import config
 import os
@@ -8,57 +9,78 @@ import os
 config.load_kube_config()
 
 # https://34.42.225.108
-# creds_client_id = os.environ.get("creds_client_id")
-# creds_client_secret = os.environ.get("creds_client_secret")
+# credsOp_client_id = os.environ.get("CLIENT_ID")
+# credsOp_client_secret = os.environ.get("CLIENT_SECRET")
 # url = os.environ.get("KEYCLOAK_BASE")
 # realm = os.environ.get("KEYCLOAK_REALM")
 
-creds_client_id = "credsop"
-creds_client_secret =  "U447ybPAq3zZe1Lv7ys2oCajmcz4p3ce"
+# Basic configuration
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('CredentialsOperator')
+logger.setLevel(logging.INFO)
+logger.info(f'Logging set to %s', logging.INFO)
+
+
+credsOp_client_id = "credsop"
+credsOp_client_secret =  "U447ybPAq3zZe1Lv7ys2oCajmcz4p3ce"
 url = "http://34.173.174.235:8083/auth"
 realm = "odari"
 
 
-r1 = requests.post(
-        url + "/realms/"+ realm +"/protocol/openid-connect/token",
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data={
-                "client_id": creds_client_id,
-                "client_secret":creds_client_secret,
-                "grant_type": "client_credentials",
+try:
+    r = requests.post(
+            url + "/realms/"+ realm +"/protocol/openid-connect/token",
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data={
+                    "client_id": credsOp_client_id,
+                    "client_secret":credsOp_client_secret,
+                    "grant_type": "client_credentials",
             },
         )
+    r.raise_for_status()
+    token = r.json()["access_token"]
+except requests.HTTPError as e:
+    raise RuntimeError(
+        f"request for token failed with HTTP status {r.status_code}: {e}"
+    ) from None
+else:
+    logger.info( f'token : {token}' )
 
-token = r1.json()["access_token"]
 
-client_id = creds_client_id
+client_id = credsOp_client_id
 
-r2 = requests.get(
-        url + "/admin/realms/" + realm+ "/clients",
-        params={"clientId": client_id},
-        headers={"Authorization": "Bearer " + token},
+try:
+    r = requests.get(
+            url + "/admin/realms/" + realm + "/clients",
+            params={"clientId": client_id},
+            headers={"Authorization": "Bearer " + token},
         )
         
-client_secret = r2.json()[0]["secret"]
+    client_secret = r.json()[0]["secret"]
+except requests.HTTPError as e:
+    raise RuntimeError(
+        f"request for client_secret failed with HTTP status {r.status_code}: {e}"
+    ) from None
+else:
+    logger.info( f'client_secret : {client_secret}' )
+   
 
-print("client_secret : "+ client_secret)
+# encoded_client_id = base64.b64encode(client_id.encode('utf-8')).decode('utf-8')
+# encoded_client_secret = base64.b64encode(client_secret.encode('utf-8')).decode('utf-8')
 
 
-encoded_client_id = base64.b64encode(client_id.encode('utf-8')).decode('utf-8')
-encoded_client_secret = base64.b64encode(client_secret.encode('utf-8')).decode('utf-8')
+# # decoded_client_id = base64.b64decode(encoded_client_id).decode('utf-8')
 
+# print(encoded_client_id + "\n" + encoded_client_secret)
 
-decoded_client_id = base64.b64decode(encoded_client_id).decode('utf-8')
+# core_v1_api = kubernetes.client.CoreV1Api()
 
-print(encoded_client_id + "\n" + encoded_client_secret)
+# secret = kubernetes.client.V1Secret(
+#     metadata=kubernetes.client.V1ObjectMeta(name=client_id + "-secret"),
+#     data={"client_id": encoded_client_id, "client_secret": encoded_client_secret}  # Base64 encoded values
+# )
 
-core_v1_api = kubernetes.client.CoreV1Api()
-
-secret = kubernetes.client.V1Secret(
-    metadata=kubernetes.client.V1ObjectMeta(name=client_id + "-secret"),
-    data={"client_id": encoded_client_id, "client_secret": encoded_client_secret}  # Base64 encoded values
-)
-
-core_v1_api.create_namespaced_secret(namespace="default", body=secret)
+# core_v1_api.create_namespaced_secret(namespace="default", body=secret)
